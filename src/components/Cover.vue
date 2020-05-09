acc29262eda3f8c9a78c9bf6ac0cdcd89c125f45
braisv <brais.vidal.rodriguez@gmail.com>
2020-05-09 21:01:44 +0200

    animation cover main

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -3,8 +3,8 @@
     <div class="cover__content">
       <img src="@/assets/logo.svg" alt="Logo" class="cover__content-img">
       <div class="cover__content-text">
-        <h1 class="heading-primary--main">Brais Vidal</h1>
-        <h4 class="heading-primary--sub">Web developer</h4>
+        <h1 class="heading-primary heading-primary--main">Brais Vidal</h1>
+        <h4 class="heading-primary heading-primary--sub">Web developer</h4>
       </div>
     </div>
   </div>
